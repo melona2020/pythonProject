3dd0c7755dcbaf7abd2d32571c9967e2b25e5675
melona2020 <miki.sh@live.com>
2021-11-05 16:55:16 -0400

    file name change

--- practice.py
+++ practice.py
@@ -8,3 +8,27 @@ def tri_recursion(k):
 
 print("\n\nRecursion Example Results")
 tri_recursion(6)
+
+
+#!/bin/bash
+echo '#### Create python3 Virtual enviroment ####'
+source scl_souce enable rh-python36
+
+VIRTUAL_ENV_NAME='virtual-enviroment'
+python -m venv $VIRTUAL_ENV_NAME
+
+echo '#### Activate Virtual Enviroment ####'
+source $VIRTUAL_ENV_NAME/bin/activate
+
+echo '#### Install reqirements ####'
+pip install -r ./requirements.txt
+
+#Jump to that path
+cd /var/lib/jenkins/workspace/$JOB_NAME/
+
+echo '#### Run Tests ####'
+pytest mytest --junitxmals./xmlReport/output.xml
+
+echo '#### deactivate virtual enviroment and exit scl ####'
+deactivate
+exit
