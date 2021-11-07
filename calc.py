5f5994bd499fb97bfb9808d71fc7893fc77bce22
melona2020 <miki.sh@live.com>
2021-11-07 18:53:03 -0500

    file name change

---
+++ calc.py
@@ -0,0 +1,19 @@
+
+def add(x,y):
+    """Add function"""
+    return x + y
+def subtract(x,y):
+    """Subtract function"""
+    return x - y
+def multiply(x,y):
+    """Multiply function"""
+    return x * y
+def divide(x,y):
+    """Divide function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y
+print(add(10,5))
+print(subtract(10,5))
+print(multiply(10,5))
+print(divide(10,5))
